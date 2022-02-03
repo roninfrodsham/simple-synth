c3a67ca07b4de0a8a388504d2dd155fd9048ba84
Jonny Frodsham <jonny.frodsham@GB-MAN-WKS0828.local>
2022-02-03 21:02:24 +0000

    add button to fix autoplay

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
 
 function App() {
-  function playSound(oscType: OscillatorType) {
-    const ctx = new AudioContext()
-    const o = ctx.createOscillator()
-    o.type = oscType
-    o.connect(ctx.destination)
-    o.start()
-    o.stop(2)
-  }
+  const ctx = new (window.AudioContext || (window as any).webkitAudioContext)()
+
+  const osc = ctx.createOscillator()
+
+  osc.connect(ctx.destination)
+
+
+
+  console.log(ctx.state)
+
 
   return (
     <div className="App">
-      <button onClick={() => playSound("sine")}>Play Sine wave</button>
-      <button onClick={() => playSound("square")}>Play Square wave</button>
-      <button onClick={() => playSound("triangle")}>Play Triangle wave</button>
-      <button onClick={() => playSound("sawtooth")}>Play Sawtooth wave</button>
-    </div>
+      <button onClick={() => {
+        osc.start()
+        osc.stop(2)
+        osc.onended = () => {
+          console.log(ctx.state)
+        }
+      }}> Start</button >
+    </div >
   )
 }
+
+// function App() {
+//   let ctx = new (window.AudioContext || (window as any).webkitAudioContext)()
+
+//   function playSoundWave(oscType: OscillatorType) {
+//     const o = ctx.createOscillator()
+//     o.type = oscType
+//     o.connect(ctx.destination)
+//     o.start()
+//     o.stop(2)
+//   }
+
+//   function Oscillator(this: any, frequency: number, detune: number) {
+//     this.osc = ctx.createOscillator()
+//     this.osc.type = 'sawtooth'
+//     this.osc.frequency.value = frequency
+//     this.osc.detune.value = detune
+//     this.osc.connect(ctx.destination)
+//     this.osc.start()
+//     this.osc.stop(3)
+//   }
+
+//   return (
+//     <div className="App">
+//       <button onClick={() => {
+//         const osc1 = new Oscillator(440, 0);
+//         const osc2 = new Oscillator(440, 15);
+//       }
+//       }>Turn on synth</button>
+//     </div>
+//   )
+// }
 
 export default App
